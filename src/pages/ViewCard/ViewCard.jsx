95d5fe9eb072a7f863b99b2e7b63f006acdf757e
poofy25 <pandamagxd@gmail.com>
2023-10-15 14:36:07 +0300

    fixed card picture not showing in the vcf file

--- src/pages/ViewCard/ViewCard.jsx
+++ src/pages/ViewCard/ViewCard.jsx
@@ -1,89 +1,76 @@
 import styles from './viewCard.module.css'
-
-
 import { useParams } from "react-router-dom";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../../firebase/firebase";
 import { storage } from "../../firebase/firebase";
-import { ref , getDownloadURL } from "firebase/storage";
+import { ref , getDownloadURL} from "firebase/storage";
 import { useEffect , useState } from "react";
-
-const contactEx = {
-  number:null,
-  name:null,
-  img:null,
-  email:null
-}
+import getBase64FromUrl from './getBase64';
 
 
 function ViewCardPage() {
 
     const params = useParams()
-    console.log(params.id)
     const [cardData , setCardData] = useState(null)
     const [imgRef , setImgRef] = useState(null)
     const [imgUrl , setImgUrl] = useState(null)
+    const [imgBase64 , setImgBase64] = useState(null)
     const [contactData , setContactData] = useState(null)
 
 
    const saveToContacts = ()=>{
     if(contactData!==null){
-      console.log('it should work')
-    var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:"
-     + contactData.name + 
-     "\nTEL;TYPE=work,voice:" + contactData.number 
-     + "\nEMAIL:" + contactData.email 
-    // + `\nPHOTO;TYPE=JPEG;ENCODING=b:[${imgUrl}]`
-     + `\nPHOTO;JPEG:${imgUrl + '.jpeg'}`
-     + `\nTITLE:${contactData.title}`
-     + "\nEND:VCARD";
+       //Creating the vCard information
+       var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:"
+        + contactData.name + 
+        "\nTEL;TYPE=work,voice:" + contactData.number 
+        + "\nEMAIL:" + contactData.email 
+        + `\nPHOTO;TYPE=JPEG;ENCODING=b:[${imgBase64}]`
+        + `\nTITLE:${contactData.title}`
+        + "\nEND:VCARD";
 
 
 
-    var blob = new Blob([vcard], { type: "text/vcard" });
-    var url = URL.createObjectURL(blob);
-    
-    const newLink = document.createElement('a');
-    newLink.download = contactData.name + ".vcf";
-    newLink.textContent = contactData.name;
-    newLink.href = url;
-      console.log(newLink)
-    newLink.click();
+        var blob = new Blob([vcard], { type: "text/vcard" });
+        var url = URL.createObjectURL(blob);
+        
+        const newLink = document.createElement('a');
+        newLink.download = contactData.name + ".vcf";
+        newLink.textContent = contactData.name;
+        newLink.href = url;
+        newLink.click();
     }
-
    }
 
 
 
-
+    //Fethces the card data from the database
     useEffect(()=>{
 
         async function getDataFromId (){
             const docRef = doc(db, "cards", params.id);
             const docSnap = await getDoc(docRef);
             if (docSnap.exists()) {
-                console.log("Document data:", docSnap.data());
+               
                 setCardData(docSnap.data())
               } else {
-                // docSnap.data() will be undefined in this case
                 console.log("No such document!");
               }
         }
        getDataFromId()
     },[])
-
+    //Settings image reference from storage after getting the card data
     useEffect(()=>{
       if(cardData!==null){
        setImgRef(ref(storage, `cardImages/${cardData.cardData.imgUUID}`))
       }
     },[cardData])
-
+    //Fetch the image url from the image reference
     useEffect(()=>{
       
       if(imgRef !== null){
       getDownloadURL(imgRef)
       .then((url) => {
-        console.log('IMG URL : ' , url)
         setImgUrl(url)
       })
       .catch((error) => {
@@ -93,20 +80,33 @@ function ViewCardPage() {
     }
 
     },[imgRef])
-    //sets contactData 
+    //Transform the image url to base64 for the vcf.file
     useEffect(()=>{
-      if(imgUrl!==null){
-      setContactData({
-        name:cardData.cardData.Name,
-        number:cardData.cardData.PhoneNumber,
-        email:cardData.cardData.Email,
-        img:imgUrl,
-        title:cardData.cardData.Title
-      })
 
-    }
+     if(imgUrl!==null){
+        var base64Img 
+        async function getBase64(){
+           await getBase64FromUrl(imgUrl).then((result)=>{
+              base64Img = result.replace("data:image/jpeg;base64,", "");
+              setImgBase64(base64Img)
+           })
+        }
+        getBase64()
+     }
 
     },[imgUrl])
+    //Sets contactData 
+    useEffect(()=>{
+      if(imgUrl!==null){
+        setContactData({
+          name:cardData.cardData.Name,
+          number:cardData.cardData.PhoneNumber,
+          email:cardData.cardData.Email,
+          img:imgUrl,
+          title:cardData.cardData.Title
+        })
+      }
+    },[imgBase64])
 
     if(cardData){
         return (
