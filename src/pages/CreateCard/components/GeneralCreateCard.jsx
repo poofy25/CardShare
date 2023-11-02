013296c29bff43292d62ccc3fe566f7663b1ebf4
poofy25 <pandamagxd@gmail.com>
2023-11-02 09:07:32 +0200

    made General Data Fields more user fiendly

--- src/pages/CreateCard/components/GeneralCreateCard.jsx
+++ src/pages/CreateCard/components/GeneralCreateCard.jsx
@@ -7,44 +7,57 @@ function GeneralCreateCard(props) {
    const status = props.status
 
 
+   const onClearField = (e)=>{
+      e.target.nextElementSibling.value = ''
+
+   }
+
+
 
     return (  
 <div className={`${styles.generalEdit} ${status==='active' && styles.active}`}>
-     <h1>General</h1>
 
      <form id='generalForm' >
 
 
-
+            <span className={styles.fieldHeader}>Card Name</span>
 
             <label>
-               Card Name *
-               <input name="cardname" type="text"  required onInvalid={()=>props.changeActiveComponent(props.cardNavComponents.general , null , document.getElementById('generalCreateBtn') )} />
+               <button type='button' className={styles.clearFieldBtn} onClick={onClearField}>X</button>
+               <input name="cardname" placeholder='Enter Card Name' type="text" defaultValue='New Card'  required onInvalid={()=>props.changeActiveComponent(props.cardNavComponents.general , null , document.getElementById('generalCreateBtn') )} />
             </label>
+
+            <span className={styles.fieldHeader}>Personal</span>
 
             <label>
                Fullname *
+               <button type='button' className={styles.clearFieldBtn} onClick={onClearField}>X</button>
                <input name="fullname" type="text" required onInvalid={()=>props.changeActiveComponent(props.cardNavComponents.general , null , document.getElementById('generalCreateBtn') )} />
             </label>
 
             <label>
                Phone Number
-               <input name="phone" type="number" />
+               <button type='button' className={styles.clearFieldBtn} onClick={onClearField}>X</button>
+               <input name="phone" type="tel" />
             </label>
+
+            <span className={styles.fieldHeader}>Affiliate</span>
 
             <label>
                Title
+               <button type='button' className={styles.clearFieldBtn} onClick={onClearField}>X</button>
                <input name="title" type="text"  />
             </label>
 
             <label>
                Company
+               <button type='button' className={styles.clearFieldBtn} onClick={onClearField}>X</button>
                <input name="company" type="text" />
             </label>
 
             <label>
                Notes
-               <input name="headline" type="text"/>
+               <textarea name="headline" />
             </label>
 
      </form>
